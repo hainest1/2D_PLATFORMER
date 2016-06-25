48d6356808a429e2a944e7690612d17fcc4ea9ed
Tom <hainest1@nku.edu>
2016-06-24 22:54:17 -0400

    FIRST COMMIT

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerControl : MonoBehaviour {
+
+    public float jumpForce = 10.0f;
+    public float horizontalSpeed = 15.0f;
+    public float maxVelocity = 100.0f;
+
+    // Use this for initialization
+    void Start ()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (GetComponent<Rigidbody2D>().velocity.x <= maxVelocity * 0.3f)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(10 * horizontalSpeed * Time.deltaTime, 0));
+        }
+        else if (GetComponent<Rigidbody2D>().velocity.x <= maxVelocity)
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(horizontalSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, jumpForce * Time.deltaTime));
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Wall")
+        {
+            GameOver(0.0f);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 0);
+        }
+        if (other.tag == "End")
+        {
+            float yValue = transform.position.y;
+            GameOver(yValue);
+        }
+    }
+
+    void GameOver(float y)
+    {
+        transform.position = new Vector3(0, y, 0);
+        GetComponent<TrailRenderer>().Clear();
+        //GetComponent<Rigidbody2D>().velocity = new Vector2(0,0);
+        //GetComponent<Rigidbody2D>().Reset
+
+    }
+}
