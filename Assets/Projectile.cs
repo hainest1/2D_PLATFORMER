f49b1b4217299032d5243b9552e108f2c6542d05
Tom <hainest1@nku.edu>
2016-06-29 15:36:10 -0400

    Moved player to left of center Added nuke

--- Assets/Projectile.cs
+++ Assets/Projectile.cs
@@ -3,38 +3,60 @@ using System.Collections;
 
 public class Projectile : MonoBehaviour {
 
-    public Transform projectile;
-    public Transform spawnLocation;
-    public float bulletForce = 200.0f;
-    public float shotCooldown = 1.0f;
+    public Transform rocketProjectile;
+    public Transform rocketSpawnLocation;
+    public Transform bombProjectile;
+    public Transform bombSpawnLocation;
+    public float rocketForce = 50.0f;
+    public float bombForce = 10.0f;
+    public float shotOneCooldown = 1.0f;
+    public float shotTwoCooldown = 1.0f;
 
-    private bool shootCD = false;
+    private bool shoot1CD = false;
+    private bool shoot2CD = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetButtonDown("Fire1") && !shootCD)
+        if (Input.GetButtonDown("Fire1") && !shoot1CD)
             Shoot();
+        if (Input.GetButtonDown("Fire2") && !shoot2CD)
+            DropBomb();
 
     }
 
     void Shoot()
     {
         Transform clone;
-        clone = Instantiate(projectile, spawnLocation.position, spawnLocation.rotation) as Transform;
-        shootCD = true;
+        clone = Instantiate(rocketProjectile, rocketSpawnLocation.position, rocketSpawnLocation.rotation) as Transform;
+        shoot1CD = true;
         PlayShootSound();
         //Collider2D cloneCollider = clone.GetComponent<Collider2D>();
         //Collider2D myCollider = GetComponent<Collider2D>();
         // Add force to the cloned object in the object's forward direction
         // Physics.IgnoreCollision(myCollider, cloneCollider);
-        clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(bulletForce, 0), ForceMode2D.Impulse);
+        clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(rocketForce, 0), ForceMode2D.Impulse);
         Destroy (clone.gameObject, 5.0f);
-        Invoke("ResetShootCD", shotCooldown);
+        Invoke("ResetShoot1CD", shotOneCooldown);
+    }
+
+    void DropBomb()
+    {
+        Transform clone;
+        clone = Instantiate(bombProjectile, bombSpawnLocation.position, bombSpawnLocation.rotation) as Transform;
+        shoot2CD = true;
+        PlayShootSound();
+        Transform player = GameObject.FindGameObjectWithTag("Player").transform;
+        // TODO: Change this to only set the X component of velocity for the bomb
+        clone.GetComponent<Rigidbody2D>().velocity = player.GetComponent<Rigidbody2D>().velocity;
+        clone.GetComponent<Rigidbody2D>().angularVelocity = player.GetComponent<Rigidbody2D>().angularVelocity;
+        clone.GetComponent<Rigidbody2D>().AddForce(new Vector2(bombForce, 0), ForceMode2D.Impulse);
+        Destroy(clone.gameObject, 10.0f);
+        Invoke("ResetShoot2CD", shotTwoCooldown);
     }
 
     void PlayShootSound()
@@ -42,8 +64,13 @@ public class Projectile : MonoBehaviour {
         this.GetComponent<AudioSource>().Play();
     }
 
-    void ResetShootCD()
+    void ResetShoot1CD()
     {
-        shootCD = false;
+        shoot1CD = false;
+    }
+
+    void ResetShoot2CD()
+    {
+        shoot2CD = false;
     }
 }
